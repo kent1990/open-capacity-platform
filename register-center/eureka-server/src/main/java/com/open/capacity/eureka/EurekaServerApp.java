105f7f7f3140d99b2da8d7443bb2197a225ae3b7
Persie <1930924512@qq.com>
2018-11-30 11:06:51 +0800

    a

--- register-center/eureka-server/src/main/java/com/open/capacity/eureka/EurekaServerApp.java
+++ register-center/eureka-server/src/main/java/com/open/capacity/eureka/EurekaServerApp.java
@@ -1,12 +1,10 @@
 package com.open.capacity.eureka;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
-import org.springframework.context.ConfigurableApplicationContext;
 
 /** 
 * @author 作者 owen E-mail: 624191343@qq.com
@@ -23,12 +21,14 @@ public class EurekaServerApp {
 	public static void main(String[] args) {
 
 //    	1本地启动采用此方法加载profiles文件
-//		ConfigurableApplicationContext context = new SpringApplicationBuilder(UnieapEurekaServerApplication.class).
-//				profiles("slave3").run(args);
+//    	ConfigurableApplicationContext context = new SpringApplicationBuilder(EurekaServerApp.class).
+//				profiles("slave0").run(args);
+		SpringApplication.run(EurekaServerApp.class, args);
 //    	2服务器采用此方法 java -jar   --spring.profiles.active=slave3;
-//    	 SpringApplication.run(DreiEurekaServerApp.class, args);
-    	ConfigurableApplicationContext context = new SpringApplicationBuilder(EurekaServerApp.class).
-				profiles("slave0").run(args);
+//    	 SpringApplication.run(EurekaServerApp.class, args);
+
+    	
+    	
 	}
 
 }
