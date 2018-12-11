0e470542678971b8a1a7be8c368ab739a3f137f0
Persie <1930924512@qq.com>
2018-12-11 23:16:21 +0800

    a

---
+++ config-center/apollo/apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/PortalApplication.java
@@ -0,0 +1,25 @@
+package com.ctrip.framework.apollo.portal;
+
+import com.ctrip.framework.apollo.common.ApolloCommonConfig;
+import com.ctrip.framework.apollo.openapi.PortalOpenApiConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableAspectJAutoProxy
+@Configuration
+@PropertySource(value = {"classpath:portal.properties"})
+@EnableAutoConfiguration
+@EnableTransactionManagement
+@ComponentScan(basePackageClasses = {ApolloCommonConfig.class,
+    PortalApplication.class, PortalOpenApiConfig.class})
+public class PortalApplication {
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(PortalApplication.class, args);
+  }
+}
