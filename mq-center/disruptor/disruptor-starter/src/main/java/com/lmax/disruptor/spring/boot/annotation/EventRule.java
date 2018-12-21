4fd4fefc3331e803d1f1bb45869386d9b4cfd295
Persie <1930924512@qq.com>
2018-12-21 10:05:55 +0800

    a

---
+++ mq-center/disruptor/disruptor-starter/src/main/java/com/lmax/disruptor/spring/boot/annotation/EventRule.java
@@ -0,0 +1,21 @@
+package com.lmax.disruptor.spring.boot.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented	
+@Inherited		
+public @interface EventRule {
+	
+	/**
+	 * Ant风格的事件分发规则表达式,格式为：/event/tags/keys，如：/Event-DC-Output/TagA-Output/**
+	 */
+	String value() default "*";
+	
+}
---
+++ mq-center/disruptor/disruptor-starter/src/main/java/com/lmax/disruptor/spring/boot/event/DisruptorEvent.java
@@ -0,0 +1,93 @@
+package com.lmax.disruptor.spring.boot.event;
+
+import java.util.EventObject;
+
+/**
+ * 
+ * @className	： DisruptorEvent
+ * @description	： 事件(Event) 就是通过 Disruptor 进行交换的数据类型。
+ * @author 		： <a href="https://github.com/vindell">vindell</a>
+ * @date		： 2017年4月20日 下午9:29:21
+ * @version 	V1.0
+ */
+@SuppressWarnings("serial")
+public abstract class DisruptorEvent extends EventObject {
+
+	/** System time when the event happened */
+	private final long timestamp;
+	/** Event Name */
+	private String event;
+	/** Event Tag */
+	private String tag;
+	/** Event Keys */
+	private String key;
+	/** Event body */
+	private Object body;
+	
+	/**
+	 * Create a new ConsumeEvent.
+	 * @param source the object on which the event initially occurred (never {@code null})
+	 */
+	public DisruptorEvent(Object source) {
+		super(source);
+		this.timestamp = System.currentTimeMillis();
+	}
+
+	/**
+	 * Return the system time in milliseconds when the event happened.
+	 */
+	public final long getTimestamp() {
+		return this.timestamp;
+	}
+	
+	public String getRouteExpression() {
+		return new StringBuilder("/").append(getEvent()).append("/").append(getTag()).append("/")
+				.append(getKey()).toString();
+		
+	}
+	
+	public void setSource(Object source){
+		this.source = source;
+	}
+
+	public String getEvent() {
+		return event;
+	}
+
+	public void setEvent(String event) {
+		this.event = event;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public Object getBody() {
+		return body;
+	}
+
+	public void setBody(Object body) {
+		this.body = body;
+	}
+
+	@Override
+	public String toString() {
+		return "DisruptorEvent [timestamp=" + timestamp + ", event=" + event + ", tag=" + tag + ", key=" + key
+				+ ", body=" + body + "]";
+	}
+	
+	
+	
+}
