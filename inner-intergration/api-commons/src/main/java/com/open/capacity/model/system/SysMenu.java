f27f21b654a989864d1f9efb9db7b63510afa19a
Persie <1930924512@qq.com>
2019-03-03 21:35:51 +0800

    aa

--- inner-intergration/api-commons/src/main/java/com/open/capacity/model/system/SysMenu.java
+++ inner-intergration/api-commons/src/main/java/com/open/capacity/model/system/SysMenu.java
@@ -5,13 +5,16 @@ import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
 import lombok.Data;
 
 @Data
 public class SysMenu implements Serializable {
 
 	private static final long serialVersionUID = 749360940290141180L;
-
+	@JsonSerialize(using=ToStringSerializer.class)
 	private Long id;
 	private Long parentId;
 	private String name;
--- inner-intergration/api-commons/src/main/java/com/open/capacity/model/system/SysRole.java
+++ inner-intergration/api-commons/src/main/java/com/open/capacity/model/system/SysRole.java
@@ -3,6 +3,9 @@ package com.open.capacity.model.system;
 import java.io.Serializable;
 import java.util.Date;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
 import lombok.Data;
 
 /**
@@ -17,6 +20,7 @@ public class SysRole implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 4497149010220586111L;
+	@JsonSerialize(using=ToStringSerializer.class)
 	private Long id;
 	private String code;
 	private String name;
