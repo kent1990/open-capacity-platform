7e4c21b62652bfd5a99f5b78a20b48d43103e592
zhangzhiguang <zhangzhiguang>
2019-08-29 17:17:46 +0800

    短信登录

--- inner-intergration/common-spring-boot-starter/src/main/java/com/open/capacity/common/util/StringUtils.java
+++ inner-intergration/common-spring-boot-starter/src/main/java/com/open/capacity/common/util/StringUtils.java
@@ -1,19 +1,4 @@
-/**
- * Copyright(C) 2012-2014 GuangZhou zdnst Co., Ltd. All Rights Reserved.<br/>
- * 版权所有(C)2012-2014 <br/>
- * 公司名称：广州支点科技有限公司<br/>
- * 公司地址：广州市天河区天源路401号之三E2栋<br/>
- * 网址：http://www.100100system.com/<br/>
- * <p>标        题：</p>
- * <p>文  件  名：com.zdnst.common.utils.StringUtils.java</p>
- * <p>部        门：研发一部
- * <p>版        本： 1.0</p>
- * <p>Compiler: JDK1.6.0_21</p>
- * <p>创  建  者：kaiqiang.wu</p>
- * <p>创建时间：2015年9月15日上午9:53:33</p>
- * <p>修  改  者：</p>
- * <p>修改时间：</p>
- */
+
 package com.open.capacity.common.util;
 
 import java.io.PrintWriter;
