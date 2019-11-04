3bc357e1db0a5d1c2b99d8233a557b49761d3172
someday <somedaymail@163.com>
2019-11-04 15:38:42 +0800

    优化日志

--- inner-intergration/common-spring-boot-starter/src/main/java/com/open/capacity/common/hystrix/HystrixConcurrencyStrategyConfig.java
+++ inner-intergration/common-spring-boot-starter/src/main/java/com/open/capacity/common/hystrix/HystrixConcurrencyStrategyConfig.java
@@ -1,6 +1,5 @@
 package com.open.capacity.common.hystrix;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 public class HystrixConcurrencyStrategyConfig {
  
 	@Bean
-	@ConditionalOnProperty(name = "feign.hystrix.enabled", havingValue="true")
 	public RequestAttributeHystrixConcurrencyStrategy requestAttributeHystrixConcurrencyStrategy() {
 		return new RequestAttributeHystrixConcurrencyStrategy();
 	}
