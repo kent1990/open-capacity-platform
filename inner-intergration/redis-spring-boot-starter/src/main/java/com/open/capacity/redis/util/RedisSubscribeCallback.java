5cc752247c9d6a6ac581ef869cad3c6237b97b00
wubin <wubinsit@163.com>
2019-11-09 22:40:59 +0800

    oracle版本

--- inner-intergration/redis-spring-boot-starter/src/main/java/com/open/capacity/redis/util/RedisSubscribeCallback.java
+++
@@ -1,6 +0,0 @@
-package com.open.capacity.redis.util;
-
- 
-public interface RedisSubscribeCallback {
-    void callback(String msg);
-}
--- inner-intergration/uaa-server-spring-boot-starter/src/main/java/com/open/capacity/uaa/server/json/JsonMapper.java
+++
@@ -1,7 +0,0 @@
-package com.open.capacity.uaa.server.json;
-
-public interface JsonMapper {
-	String write(Object input) throws Exception;
-
-	<T> T read(String input, Class<T> type) throws Exception;
-}
