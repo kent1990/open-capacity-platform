9b7c9eac78f6b61269e821bb553d64163a65e23e
someday <somedaymail@163.com>
2019-11-19 22:02:10 +0800

    追加异常捕获

--- inner-intergration/uaa-client-spring-boot-starter/src/main/java/com/open/capacity/uaa/client/config/ExceptionHandlerAdvice.java
+++ inner-intergration/uaa-client-spring-boot-starter/src/main/java/com/open/capacity/uaa/client/config/ExceptionHandlerAdvice.java
@@ -3,16 +3,26 @@ package com.open.capacity.uaa.client.config;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.validation.ConstraintViolationException;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.open.capacity.common.exception.controller.ControllerException;
+import com.open.capacity.common.exception.dao.DataAccessException;
 import com.open.capacity.common.exception.hystrix.HytrixException;
-
- 
+import com.open.capacity.common.exception.service.ServiceException;
 
 /**
  * @author 作者 owen E-mail: 624191343@qq.com
@@ -53,6 +63,117 @@ public class ExceptionHandlerAdvice {
 		return data;
 	}
 
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public Map<String, Object> handleError(MissingServletRequestParameterException e) {
+		Map<String, Object> data = new HashMap<>();
+		data.put("resp_code", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		data.put("resp_msg", e.getMessage());
+
+		return data;
+	}
+
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public Map<String, Object> handleError(MethodArgumentTypeMismatchException e) {
+		Map<String, Object> data = new HashMap<>();
+		data.put("resp_code", HttpStatus.FORBIDDEN.value());
+		data.put("resp_msg", e.getMessage());
+
+		return data;
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public Map<String, Object> handleError(MethodArgumentNotValidException e) {
+		Map<String, Object> data = new HashMap<>();
+		data.put("resp_code", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		data.put("resp_msg", e.getMessage());
+
+		return data;
+	}
+
+	@ExceptionHandler(BindException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public Map<String, Object> handleError(BindException e) {
+		Map<String, Object> data = new HashMap<>();
+		data.put("resp_code", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		data.put("resp_msg", e.getMessage());
+
+		return data;
+	}
+
+	@ExceptionHandler(ConstraintViolationException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public Map<String, Object> handleError(ConstraintViolationException e) {
+		Map<String, Object> data = new HashMap<>();
+		data.put("resp_code", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		data.put("resp_msg", e.getMessage());
+
+		return data;
+	}
+
+	@ExceptionHandler(NoHandlerFoundException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public Map<String, Object> handleError(NoHandlerFoundException e) {
+		Map<String, Object> data = new HashMap<>();
+		data.put("resp_code", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		data.put("resp_msg", e.getMessage());
+
+		return data;
+	}
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public Map<String, Object> handleError(HttpMessageNotReadableException e) {
+		Map<String, Object> data = new HashMap<>();
+		data.put("resp_code", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		data.put("resp_msg", e.getMessage());
+
+		return data;
+	}
+
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public Map<String, Object> handleError(HttpRequestMethodNotSupportedException e) {
+		Map<String, Object> data = new HashMap<>();
+		data.put("resp_code", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		data.put("resp_msg", e.getMessage());
+
+		return data;
+	}
+
+	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public Map<String, Object> handleError(HttpMediaTypeNotSupportedException e) {
+		Map<String, Object> data = new HashMap<>();
+		data.put("resp_code", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		data.put("resp_msg", e.getMessage());
+
+		return data;
+	}
+
+	@ExceptionHandler({ DataAccessException.class })
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public Map<String, Object> dataAccessException(DataAccessException exception) {
+		Map<String, Object> data = new HashMap<>();
+		data.put("resp_code", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		data.put("resp_msg", exception.getMessage());
+
+		return data;
+
+	}
+
+	@ExceptionHandler({ ServiceException.class })
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public Map<String, Object> serviceException(ServiceException exception) {
+		Map<String, Object> data = new HashMap<>();
+		data.put("resp_code", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		data.put("resp_msg", exception.getMessage());
+
+		return data;
+	}
+
 	@ExceptionHandler({ ControllerException.class })
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	public Map<String, Object> controllerException(ControllerException exception) {
@@ -72,13 +193,14 @@ public class ExceptionHandlerAdvice {
 
 		return data;
 	}
+
 	@ExceptionHandler(Throwable.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, Object> handleError(Throwable e) {
-        Map<String, Object> data = new HashMap<>();
+	public Map<String, Object> handleError(Throwable e) {
+		Map<String, Object> data = new HashMap<>();
 		data.put("resp_code", HttpStatus.INTERNAL_SERVER_ERROR.value());
 		data.put("resp_msg", e.getMessage());
 
 		return data;
-    }
+	}
 }
