9c0c279018cf3f77fd94682750c8ac11908bd24d
lwg <lwg@DESKTOP-ERSIACG>
2019-12-06 12:30:06 +0800

    修改bug，mvc传递app_trace_id动态控制

--- inner-intergration/log-spring-boot-starter/src/main/java/com/open/capacity/log/config/LogAutoConfig.java
+++ inner-intergration/log-spring-boot-starter/src/main/java/com/open/capacity/log/config/LogAutoConfig.java
@@ -1,11 +1,8 @@
 package com.open.capacity.log.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.open.capacity.log.interceptor.LogInterceptor;
 
 /**
  * @author owen
@@ -13,21 +10,11 @@ import com.open.capacity.log.interceptor.LogInterceptor;
  * 日志拦截器，排除对spring cloud gateway的影响 (WebMvcConfigurer)
  * 
  */
-@Configuration
-@ConditionalOnClass(WebMvcConfigurer.class)
-public class LogAutoConfig implements WebMvcConfigurer {
+public class LogAutoConfig {
  
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
- 
-        /**
-         * 自定义拦截器，添加拦截路径和排除拦截路径
-         * addPathPatterns():添加需要拦截的路径
-         * excludePathPatterns():添加不需要拦截的路径
-         * 在括号中还可以使用集合的形式，如注释部分代码所示
-         */
-        registry.addInterceptor(new LogInterceptor()).addPathPatterns("/**") ;
- 
- 
-    }
+	@Bean
+	@ConditionalOnClass(WebMvcConfigurer.class)
+	public WebMvcConfigurer get() {
+		return new LogWebMvcConfig();
+	}
 }
