bd0dc72a4f2396c78a195794b2db398ae1dbf711
lwg <lwg@DESKTOP-ERSIACG>
2019-12-07 00:21:15 +0800

    去掉Configuration注解

--- inner-intergration/log-spring-boot-starter/src/main/java/com/open/capacity/log/config/LogAutoConfig.java
+++ inner-intergration/log-spring-boot-starter/src/main/java/com/open/capacity/log/config/LogAutoConfig.java
@@ -1,8 +1,10 @@
 package com.open.capacity.log.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.open.capacity.log.interceptor.LogInterceptor;
 
 /**
  * @author owen
@@ -10,11 +12,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * 日志拦截器，排除对spring cloud gateway的影响 (WebMvcConfigurer)
  * 
  */
-public class LogAutoConfig {
+@ConditionalOnClass(WebMvcConfigurer.class)
+public class LogAutoConfig implements WebMvcConfigurer {
  
-	@Bean
-	@ConditionalOnClass(WebMvcConfigurer.class)
-	public WebMvcConfigurer get() {
-		return new LogWebMvcConfig();
-	}
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+ 
+        /**
+         * 自定义拦截器，添加拦截路径和排除拦截路径
+         * addPathPatterns():添加需要拦截的路径
+         * excludePathPatterns():添加不需要拦截的路径
+         * 在括号中还可以使用集合的形式，如注释部分代码所示
+         */
+        registry.addInterceptor(new LogInterceptor()).addPathPatterns("/**") ;
+ 
+ 
+    }
 }
