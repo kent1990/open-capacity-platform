01cbdc96a4cfcaec8bd3d0d0946ba46df553e9b8
someday <somedaymail@163.com>
2019-12-17 21:47:09 +0800

    代码优化

--- inner-intergration/common-spring-boot-starter/src/main/java/com/open/capacity/common/async/ContextCopyingDecorator.java
+++ inner-intergration/common-spring-boot-starter/src/main/java/com/open/capacity/common/async/ContextCopyingDecorator.java
@@ -8,14 +8,18 @@ import org.springframework.web.context.request.RequestContextHolder;
 public class ContextCopyingDecorator implements TaskDecorator {
     @Override
     public Runnable decorate(Runnable runnable) {
-        RequestAttributes context = RequestContextHolder.currentRequestAttributes();
-        return () -> {
-            try {
-                RequestContextHolder.setRequestAttributes(context);
-                runnable.run();
-            } finally {
-                RequestContextHolder.resetRequestAttributes();
-            }
-        };
+        try {
+			RequestAttributes context = RequestContextHolder.currentRequestAttributes();
+			return () -> {
+			    try {
+			        RequestContextHolder.setRequestAttributes(context);
+			        runnable.run();
+			    } finally {
+			        RequestContextHolder.resetRequestAttributes();
+			    }
+			};
+		} catch (IllegalStateException e) {
+			return runnable;
+		}
     }
 }
