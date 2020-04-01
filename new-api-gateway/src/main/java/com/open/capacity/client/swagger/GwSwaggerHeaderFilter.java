8da46f5de54c251fd4cfc2b9075b9df0042abae6
someday <somedaymail@163.com>
2020-04-01 12:46:27 +0800

    调整swagger

--- new-api-gateway/src/main/java/com/open/capacity/client/swagger/GwSwaggerHeaderFilter.java
+++ new-api-gateway/src/main/java/com/open/capacity/client/swagger/GwSwaggerHeaderFilter.java
@@ -1,6 +1,8 @@
-package com.open.capacity.client.filter;
+package com.open.capacity.client.swagger;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -8,22 +10,23 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 
-import com.open.capacity.client.config.GatewaySwaggerProvider;
-
 @Component
 @SuppressWarnings("all")
+@EnableConfigurationProperties(SwaggerButlerProperties.class)
 public class GwSwaggerHeaderFilter extends AbstractGatewayFilterFactory {
-	 private static final String HEADER_NAME = "X-Forwarded-Prefix";
+	@Autowired
+	private SwaggerButlerProperties swaggerButlerProperties;
+    private static final String HEADER_NAME = "X-Forwarded-Prefix";
 	 
 	    @Override
 	    public GatewayFilter apply(Object config) {
 	        return (exchange, chain) -> {
 	            ServerHttpRequest request = exchange.getRequest();
 	            String path = request.getURI().getPath();
-	            if (!StringUtils.endsWithIgnoreCase(path, GatewaySwaggerProvider.API_URI)) {
+	            if (!StringUtils.endsWithIgnoreCase(path, swaggerButlerProperties.getApiDocsPath())) {
 	                return chain.filter(exchange);
 	            }
-	            String basePath = path.substring(0, path.lastIndexOf(GatewaySwaggerProvider.API_URI));
+	            String basePath = path.substring(0, path.lastIndexOf(swaggerButlerProperties.getApiDocsPath()));
 	            ServerHttpRequest newRequest = request.mutate().header(HEADER_NAME, basePath).build();
 	            ServerWebExchange newExchange = exchange.mutate().request(newRequest).build();
 	            return chain.filter(newExchange);
