2161798d270a93d7b3fd9ee4733ff8ef3c694c44
Administrator <zhangzhiguang>
2020-05-18 11:47:35 +0800

    sms Util问题

--- inner-intergration/common-spring-boot-starter/src/main/java/com/open/capacity/common/util/SysUserUtil.java
+++ inner-intergration/common-spring-boot-starter/src/main/java/com/open/capacity/common/util/SysUserUtil.java
@@ -3,6 +3,7 @@ package com.open.capacity.common.util;
 import java.util.*;
 
 import cn.hutool.core.collection.CollectionUtil;
+import com.open.capacity.common.token.SmsCodeAuthenticationToken;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -38,28 +39,34 @@ public class SysUserUtil {
             OAuth2Authentication oAuth2Auth = (OAuth2Authentication) authentication;
             authentication = oAuth2Auth.getUserAuthentication();
 
-            if (authentication instanceof UsernamePasswordAuthenticationToken) {
+            Object principal = null;
+            if (authentication instanceof UsernamePasswordAuthenticationToken ){
                 UsernamePasswordAuthenticationToken authenticationToken = (UsernamePasswordAuthenticationToken) authentication;
-
-                if (authenticationToken.getPrincipal() instanceof LoginAppUser) {
-                    return (LoginAppUser) authenticationToken.getPrincipal();
-                } else if (authenticationToken.getPrincipal() instanceof Map) {
-
-                    LoginAppUser loginAppUser = BeanUtil.mapToBean((Map) authenticationToken.getPrincipal(), LoginAppUser.class, true);
-                    Set<SysRole> roles = new HashSet<>();
-                    if (CollectionUtil.isNotEmpty(loginAppUser.getSysRoles())) {
-    					for(Iterator<SysRole> it = loginAppUser.getSysRoles().iterator(); it.hasNext();){
-    						SysRole role =  BeanUtil.mapToBean((Map) it.next() , SysRole.class, false);
-    						roles.add(role) ;
-    					}
-                    }
-                    loginAppUser.setSysRoles(roles); 
-                    return loginAppUser;
-                }
-            } else if (authentication instanceof PreAuthenticatedAuthenticationToken) {
+                principal = authenticationToken.getPrincipal();
+            }else if (authentication instanceof SmsCodeAuthenticationToken ){
+                //短信sms方式
+                SmsCodeAuthenticationToken authenticationToken = (SmsCodeAuthenticationToken)authentication;
+                principal =  authenticationToken.getPrincipal();
+            }else if (authentication instanceof PreAuthenticatedAuthenticationToken) {
                 // 刷新token方式
                 PreAuthenticatedAuthenticationToken authenticationToken = (PreAuthenticatedAuthenticationToken) authentication;
-                return (LoginAppUser) authenticationToken.getPrincipal();
+                principal = authenticationToken.getPrincipal();
+            }
+
+            if (principal instanceof LoginAppUser) {
+                    return (LoginAppUser) principal;
+            } else if (principal instanceof Map) {
+
+                LoginAppUser loginAppUser = BeanUtil.mapToBean((Map) principal, LoginAppUser.class, true);
+                Set<SysRole> roles = new HashSet<>();
+                if (CollectionUtil.isNotEmpty(loginAppUser.getSysRoles())) {
+                    for(Iterator<SysRole> it = loginAppUser.getSysRoles().iterator(); it.hasNext();){
+                        SysRole role =  BeanUtil.mapToBean((Map) it.next() , SysRole.class, false);
+                        roles.add(role) ;
+                    }
+                }
+                loginAppUser.setSysRoles(roles);
+                return loginAppUser;
             }
         }
 
