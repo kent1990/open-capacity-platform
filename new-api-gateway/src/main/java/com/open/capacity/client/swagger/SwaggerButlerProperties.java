42696a5640f406e656ad92ad44e3146d38b404f9
someday <somedaymail@163.com>
2020-05-25 17:23:09 +0800

    new-api-gateway swagger-butler

--- new-api-gateway/src/main/java/com/open/capacity/client/swagger/SwaggerButlerProperties.java
+++ new-api-gateway/src/main/java/com/open/capacity/client/swagger/SwaggerButlerProperties.java
@@ -9,7 +9,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import lombok.Data;
 
- 
+/**
+ * Created by 程序猿DD/翟永超 on 2018/5/24.
+ * <p>
+ * Blog: http://blog.didispace.com/
+ * Github: https://github.com/dyc87112/
+ */ 
 @Data
 @ConfigurationProperties("swagger.butler")
 public class SwaggerButlerProperties {
